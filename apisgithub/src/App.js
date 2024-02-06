2a36d2cb1211be141940978bfb06ee4a99c928d1
Tahaouad <tahaouad04@gmail.com>
2024-02-06 01:20:10 +0100

    link using axios

---
+++ apisgithub/src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+
+
+function App() {
+  const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+       
+                const response = await axios.get('https://raw.githubusercontent.com/Tahaouad/Api_Data/master/users');
+                setData(response.data);
+           
+            
+        };
+
+        fetchData();
+    }, []);
+  return (
+    <div className="App">
+       <>
+       
+            {data.length > 0 ? (
+                data.map((d, index) => (
+                    <div key={index}>
+                      {d.name}{'   '}{d.age}
+                    </div>
+                ))
+            ) : (
+                <p>Loading...</p>
+            )}
+        </>
+    </div>
+  );
+}
+
+export default App;
